51e1e9ca388fb0b3c691e66207adea5ae15f9288
VasylHoshovsky <vasyl.hoshovsky@gmail.com>
2015-08-28 17:13:37 +0300

    Logger implemented

--- src/main/java/ua/com/vhsoft/crman/mvc/controllers/TestController.java
+++ src/main/java/ua/com/vhsoft/crman/mvc/controllers/TestController.java
@@ -1,5 +1,6 @@
 package ua.com.vhsoft.crman.mvc.controllers;
 
+import org.apache.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,9 @@ public class TestController {
 
     @RequestMapping(value = "test", method = RequestMethod.GET)
     public String helloWorld(Model model) {
+        Logger logger = Logger.getLogger(this.getClass());
+        logger.info("!!!!!!!!!!!!!!!!!!!!!!!!!!logger message");
+        
         return "test";
     }
 }
